089f42a4c4bceb6223ee78a9b31a5c5146e73539
Wallace W <wallace_wolv@hotmail.com>
2025-10-25 23:55:06 -0300

    feat: Criando primeiras permissões

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUser = ability.can('delete', 'User')
+
+const useCannotDeleteOtherUser = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOtherUser)
+console.log(useCannotDeleteOtherUser)
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,1 +1,28 @@
-const teste = 'oi'
+import {
+  AbilityBuilder,
+  type CreateAbility,
+  createMongoAbility,
+  type ForcedSubject,
+  type MongoAbility,
+} from '@casl/ability'
+
+const actions = ['manage', 'invite', 'delete'] as const
+const subjects = ['User', 'all'] as const
+
+type AppAbilities = [
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  ),
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+
+can('manage', 'User')
+cannot('delete', 'User')
+
+export const ability = build()
