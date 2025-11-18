45345686a6254f6aa44dce9c2adb2ddb70f237f0
Wallace W <wallace_wolv@hotmail.com>
2025-11-18 00:32:47 -0300

    feat: Rota: Listagem de convites

--- apps/api/src/http/routes/invites/invites.routes.ts
+++ apps/api/src/http/routes/invites/invites.routes.ts
@@ -2,8 +2,10 @@ import type { FastifyInstance } from 'fastify'
 
 import { createInvite } from './create-invite'
 import { getInvite } from './get-invite'
+import { getInvites } from './get-invites'
 
 export function InvitesRoutes(app: FastifyInstance) {
   app.register(createInvite)
   app.register(getInvite)
+  app.register(getInvites)
 }
