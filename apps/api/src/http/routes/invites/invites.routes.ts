735afe919346f21f2373228af7c001953dd5c0f3
Wallace W <wallace_wolv@hotmail.com>
2025-11-18 00:16:27 -0300

    feat: Rota: Detalhes do convite

--- apps/api/src/http/routes/invites/invites.routes.ts
+++ apps/api/src/http/routes/invites/invites.routes.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from 'fastify'
 
 import { createInvite } from './create-invite'
+import { getInvite } from './get-invite'
 
 export function InvitesRoutes(app: FastifyInstance) {
   app.register(createInvite)
+  app.register(getInvite)
 }
