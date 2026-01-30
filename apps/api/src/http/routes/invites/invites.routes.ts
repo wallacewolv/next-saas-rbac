98413dcfe377ba8f07d0129db04fda80537fbeaa
Wallace W <wallace_wolv@hotmail.com>
2026-01-30 00:41:23 -0300

    feat: Rota: Convites pendentes

--- apps/api/src/http/routes/invites/invites.routes.ts
+++ apps/api/src/http/routes/invites/invites.routes.ts
@@ -4,6 +4,7 @@ import { acceptInvite } from './accept-invite'
 import { createInvite } from './create-invite'
 import { getInvite } from './get-invite'
 import { getInvites } from './get-invites'
+import { getPendingInvites } from './get-pending-invites'
 import { rejectInvite } from './reject-invite'
 import { revokeInvite } from './revoke-invite'
 
@@ -14,4 +15,5 @@ export function InvitesRoutes(app: FastifyInstance) {
   app.register(acceptInvite)
   app.register(rejectInvite)
   app.register(revokeInvite)
+  app.register(getPendingInvites)
 }
