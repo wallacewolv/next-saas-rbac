51df52aaa44156ac6cb89af454844514836e2908
Wallace W <wallace_wolv@hotmail.com>
2026-01-30 00:22:15 -0300

    feat: Rota: Aceitar convite

--- apps/api/src/http/routes/invites/invites.routes.ts
+++ apps/api/src/http/routes/invites/invites.routes.ts
@@ -1,5 +1,6 @@
 import type { FastifyInstance } from 'fastify'
 
+import { acceptInvite } from './accept-invite'
 import { createInvite } from './create-invite'
 import { getInvite } from './get-invite'
 import { getInvites } from './get-invites'
@@ -8,4 +9,5 @@ export function InvitesRoutes(app: FastifyInstance) {
   app.register(createInvite)
   app.register(getInvite)
   app.register(getInvites)
+  app.register(acceptInvite)
 }
