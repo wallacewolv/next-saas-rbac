159dc2c50dd6eec14bd0cd327edce01045300694
Wallace W <wallace_wolv@hotmail.com>
2026-02-01 19:13:11 -0300

    feat: Obtendo usuário autenticado

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,6 +1,28 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookieStore = await cookies()
   return cookieStore.has('token')
+}
+
+export async function auth() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+  } catch (err) {
+    console.error('Error fetching profile:', err)
+  }
+
+  redirect('/api/auth/sign-out')
 }
--- apps/web/src/http/api-client.ts
+++ apps/web/src/http/api-client.ts
@@ -1,5 +1,26 @@
+import { getCookie } from 'cookies-next'
 import ky from 'ky'
 
 export const api = ky.create({
   prefixUrl: 'http://localhost:3333',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let token: string | undefined
+
+        if (typeof window !== 'undefined') {
+          token = getCookie('token') as string | undefined
+        } else {
+          const { cookies: getServerCookies } = await import('next/headers')
+
+          const cookieStore = await getServerCookies()
+          token = cookieStore.get('token')?.value
+        }
+
+        if (token) {
+          request.headers.set('Authorization', `Bearer ${token}`)
+        }
+      },
+    ],
+  },
 })
