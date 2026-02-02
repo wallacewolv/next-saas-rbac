962609f27d1d27f4f6eb2429215b2ba943f57f1e
Wallace W <wallace_wolv@hotmail.com>
2026-02-01 22:01:58 -0300

    feat: Criando novos usuários

---
+++ apps/web/src/app/auth/sign-up/actions.ts
@@ -0,0 +1,66 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
+import { signUp } from '@/http/sign-up'
+
+const signUpSchema = z
+  .object({
+    name: z.string().refine((value) => value.split(' ').length > 1, {
+      message: 'Please, enter your full name.',
+    }),
+    email: z.email({ message: 'Please, provide a valid email address.' }),
+    password: z.string().min(6, {
+      message: 'Password should have at least 6 characters.',
+    }),
+    password_confirmation: z.string(),
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: 'Password confirmation does not match.',
+    path: ['password_confirmation'],
+  })
+
+export async function signUpAction(data: FormData) {
+  const result = signUpSchema.safeParse({
+    name: data.get('name'),
+    email: data.get('email'),
+    password: data.get('password'),
+    password_confirmation: data.get('password_confirmation'),
+  })
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { name, email, password } = result.data
+
+  try {
+    await signUp({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+      return {
+        success: false,
+        message,
+        errors: null,
+      }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
+}
