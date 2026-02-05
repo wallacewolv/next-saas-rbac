7cb5b4c08e4ece8465aade0d4fee83542cc54cf9
Wallace W <wallace_wolv@hotmail.com>
2026-02-04 23:28:33 -0300

    feat: Listando organizações

--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,6 +1,8 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
 import Link from 'next/link'
 
+import { getOrganizations } from '@/http/get-organizations'
+
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import {
   DropdownMenu,
@@ -12,7 +14,9 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 
-export function OrganizationSwitcher() {
+export async function OrganizationSwitcher() {
+  const { organizations } = await getOrganizations()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex w-42 items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
@@ -27,14 +31,23 @@ export function OrganizationSwitcher() {
       >
         <DropdownMenuGroup>
           <DropdownMenuLabel>Organizations</DropdownMenuLabel>
-          <DropdownMenuItem>
-            <Avatar className="mr-2 size-4">
-              <AvatarImage src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Acme-corp.png" />
-              <AvatarFallback />
-            </Avatar>
-            <span className="line-clamp-1">Acme Inc</span>
-          </DropdownMenuItem>
+
+          {organizations.map(({ avatarUrl, id, name, slug }) => {
+            return (
+              <DropdownMenuItem key={id} asChild>
+                <Link href={`/org/${slug}`}>
+                  <Avatar className="mr-2 size-4">
+                    {avatarUrl && <AvatarImage src={avatarUrl} />}
+                    <AvatarFallback />
+                  </Avatar>
+                  <span className="line-clamp-1">{name}</span>
+                </Link>
+              </DropdownMenuItem>
+            )
+          })}
+
           <DropdownMenuSeparator />
+
           <DropdownMenuItem asChild>
             <Link href="/create-organization">
               <PlusCircle className="mr-2 size-4" />
