d68852b7360642e39ade9fc1754641356aafeaf9
Wallace W <wallace_wolv@hotmail.com>
2026-02-04 23:12:39 -0300

    feat: Componente: organization-switcher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,5 +1,6 @@
-import { ShieldCheck } from 'lucide-react'
+import { ShieldCheck, Slash } from 'lucide-react'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
@@ -7,6 +8,10 @@ export function Header() {
     <div className="mx-auto flex max-w-300 items-center justify-between">
       <div className="flex items-center gap-3">
         <ShieldCheck className="size-6" />
+
+        <Slash className="size-3 -rotate-24 text-border" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
---
+++ apps/web/src/components/organization-switcher.tsx
@@ -0,0 +1,48 @@
+import { ChevronsUpDown, PlusCircle } from 'lucide-react'
+import Link from 'next/link'
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function OrganizationSwitcher() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex w-42 items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2 focus-visible:ring-primary">
+        <span className="text-muted-foreground">Select organization</span>
+        <ChevronsUpDown className="ml-auto size-4 text-muted-foreground" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        alignOffset={-16}
+        sideOffset={12}
+        className="w-50"
+      >
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Avatar className="mr-2 size-4">
+              <AvatarImage src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Acme-corp.png" />
+              <AvatarFallback />
+            </Avatar>
+            <span className="line-clamp-1">Acme Inc</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <Link href="/create-organization">
+              <PlusCircle className="mr-2 size-4" />
+              Create new
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
