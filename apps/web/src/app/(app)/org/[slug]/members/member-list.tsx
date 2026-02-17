5136d2fffff86ed6b4dc5c5e5bf7ffc6a02db5ba
Wallace W <wallace_wolv@hotmail.com>
2026-02-17 14:55:58 -0300

    feat: Listagem de membros

---
+++ apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
@@ -0,0 +1,90 @@
+import { organizationSchema } from '@saas/auth'
+import { ArrowLeftRight, Crown } from 'lucide-react'
+import Image from 'next/image'
+
+import { ability, getCurrentOrg } from '@/auth/auth'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import { getMembership } from '@/http/get-current-membership'
+import { getMembers } from '@/http/get-members'
+import { getOrganization } from '@/http/get-organization'
+
+export async function MemberList() {
+  const currentOrg = await getCurrentOrg()
+
+  const permissions = await ability()
+
+  const [{ membership }, { members }, { organization }] = await Promise.all([
+    getMembership(currentOrg!),
+    getMembers(currentOrg!),
+    getOrganization(currentOrg!),
+  ])
+
+  const authOrganization = organizationSchema.parse(organization)
+
+  const canTransferOwnership = permissions?.can(
+    'transfer_ownership',
+    authOrganization,
+  )
+
+  return (
+    <div className="space-y-2">
+      <h2 className="text-lg font-semibold">Members</h2>
+
+      <div className="rounded border">
+        <Table>
+          <TableBody>
+            {members.map((member) => {
+              return (
+                <TableRow key={member.id}>
+                  <TableCell className="py-2.5" style={{ width: 48 }}>
+                    <Avatar>
+                      <AvatarFallback />
+                      {member.avatarUrl && (
+                        <Image
+                          src={member.avatarUrl}
+                          width={32}
+                          height={32}
+                          alt="Avatar image"
+                          className="aspect-square size-full"
+                        />
+                      )}
+                    </Avatar>
+                  </TableCell>
+                  <TableCell className="py-2.5">
+                    <div className="flex flex-col">
+                      <span className="font-medium inline-flex items-center gap-2">
+                        {member.name}
+                        {member.userId === membership.userId && ' (me)'}
+                        {member.userId === organization.ownerId && (
+                          <span className="inline-flex items-center gap-1 text-xs text-muted-foreground">
+                            <Crown className="size-3" />
+                            Owner
+                          </span>
+                        )}
+                      </span>
+                      <span className="text-xs text-muted-foreground">
+                        {member.email}
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="py-2.5">
+                    <div className="flex items-center justify-end gap-2">
+                      {canTransferOwnership && (
+                        <Button size="sm" variant="ghost">
+                          <ArrowLeftRight className="size-4 mr2" />
+                          Transfer ownership
+                        </Button>
+                      )}
+                    </div>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
---
+++ apps/web/src/app/(app)/org/[slug]/members/page.tsx
@@ -0,0 +1,22 @@
+import { ability } from '@/auth/auth'
+
+import { Invites } from './invites'
+import { MemberList } from './member-list'
+
+export default async function Members() {
+  const permissions = await ability()
+
+  const canGetInvites = permissions?.can('get', 'Invite')
+  const canGetMembers = permissions?.can('get', 'User')
+
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Members</h1>
+
+      <div className="space-y-4">
+        {canGetInvites && <Invites />}
+        {canGetMembers && <MemberList />}
+      </div>
+    </div>
+  )
+}
